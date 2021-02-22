6e09035bbbea38bb7f83fe1c6c0bc5369347cc25
delrosariotanny <tanny.delrosario@wondersco.com>
2021-02-22 08:42:32 -0500

    Change the design

--- src/components/app/CountriesDropdown.jsx
+++ src/components/app/CountriesDropdown.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import promisedCountries from '../../api/country'
 import Dropdown from '../../ui/Dropdown';
 import Span from '../../ui/Span'
+import Spinner from '../../ui/Spinner'
 import promiseRetry from "promise-retry";
 
 const CountriesDropdown = ({ onChange, onLoad, ...props }) => {
@@ -25,7 +26,12 @@ const CountriesDropdown = ({ onChange, onLoad, ...props }) => {
             return promisedCountries()
                 .catch(retry);
         }).then(countries => {
-            const countriesAsOptions = countries.map(country => ({ value: country, label: country.name }))
+            const countriesAsOptions = countries.map(country => (
+                { value: country,
+                label: <span><img src={`https://www.countryflags.io/${country.iso2}/flat/16.png`}/> <Span>{country.name}</Span></span> }
+            )
+            )
+                .sort((a, b) => a.value.name.localeCompare(b.value.name))
             setCountries(countriesAsOptions)
 
             if (countriesAsOptions[0]) {
@@ -45,9 +51,9 @@ const CountriesDropdown = ({ onChange, onLoad, ...props }) => {
 
     return (
         <>
-            {loading && <Span>{loadingMessage}</Span>}
+            {loading && <Span><Spinner></Spinner></Span>}
             {error && <Span>{error}</Span>}
-            {!loading && !error && countries && <Dropdown options={countries} onChange={(event) => onDropdownChangeHandler(event)} {...props}></Dropdown >}
+            {!loading && !error && countries && <div style={{width: "450px"}}><Dropdown options={countries} onChange={(event) => onDropdownChangeHandler(event)} {...props}></Dropdown ></div>}
         </>
     )
 }
